b1e175dfb9b3b0c92249adad158e29663a213fb1
APorkolab <adam.porkolab@gmail.com>
2025-08-29 10:50:44 +0200

    feat(shell): Maven init + minimal entrypoint

---
+++ README.md
@@ -0,0 +1,2 @@
+# typokit-shell
+Public UI (JavaFX/CLI) for ManuSmith. Will consume typokit-engine as a dependency/binary.
---
+++ src/main/java/org/manusmith/shell/MainApp.java
@@ -0,0 +1,8 @@
+package org.manusmith.shell;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("ManuSmith Shell ready.");
+        // TODO: JavaFX UI (3 tabs) vagy kezdeti CLI, később engine integráció
+    }
+}
